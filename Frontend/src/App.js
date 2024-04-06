86436b93dd30cd29efdf1737d9ac48fcb1df5e43
nayem73 <nayem73@gmail.com>
2024-04-06 09:00:12 +0600

    removed the searchbox from Header.js

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,68 +1,27 @@
 import './App.css';
 
 // Nayem
-import {useState, useEffect} from 'react';
-import { BrowserRouter as Router, Routes, Route, BrowserRouter } from 'react-router-dom';
-import api from './api/axiosConfig';
-import Layout from './components/Layout';
-// import Home from './components/home/Home';
+//import { BrowserRouter as Router, Routes, Route, BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar';
-import Home from './components/pages/Home';
 import LoginScreen from './screens/LoginScreen';
 import RegisterScreen from './screens/RegisterScreen';
 import ReviewScreen from './screens/ReviewScreen';
-import HomeScreen from './screens/HomeScreen';
 import Header from './components/Header';
+import Home from './components/pages/Home';
 
 function App() {
-  const [carouselItems, setCarouselItems] = useState([]);
-  const [aboutUs, setAboutUs] = useState({});
-
-  const getCarouselItems = async () => {
-    try {
-      const response = await api.get('/api/carousel');
-      setCarouselItems(response.data);
-    } catch (error) {
-      console.error('Error fetching carousel items: ', error);
-    }
-  }
-    const getAboutUs = async () => {
-    try {
-      const response = await api.get('/api/aboutus');
-      setAboutUs(response.data);
-    } catch (error) {
-      console.error('Error fetching aboutUs: ', error);
-    }
-  }
-
-  useEffect(() => {
-    getCarouselItems();
-    getAboutUs();
-  }, [])
-
   return (
     <>
         <Header />
         <Routes>
-          {/* <Route path='/' element={<Home carouselItems={carouselItems} aboutUs={aboutUs}/>} /> */}
           <Route path='/' element={<Home />} />
           <Route path='/login' element={<LoginScreen />} />
           <Route path='/register' element={<RegisterScreen />} />
           <Route path='/review' element={<ReviewScreen />} />
-          <Route exact path='/' element={<HomeScreen />} />
         </Routes>
     </>
   );  
-
-  // return (
-  //   <div className='App'>
-  //     <Routes>
-  //       <Route path='/' element={<Layout/>}>
-  //         <Route path='/' element={<Home carouselItems={carouselItems} aboutUs={aboutUs}/>}/>
-  //       </Route>
-  //     </Routes>
-  //   </div>
-  // );
 }
 
 export default App;
