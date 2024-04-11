508c1438f42241442d9861ca36aee5ce5d05adc3
nayem73 <nayem73@gmail.com>
2024-04-11 17:26:45 +0600

    designing the front (Cards.js)

--- Frontend/src/components/Cards.js
+++ Frontend/src/components/Cards.js
@@ -1,3 +1,4 @@
+// Cards.js file
 import React from 'react';
 import './Cards.css';
 import CardItem from './CardItem';
@@ -10,26 +11,26 @@ function Cards() {
 
   return (
     <div className='cards'>
-      <h1>At B-JET</h1>
-      <p style={{ textAlign: 'center', fontSize: '1.2rem', fontWeight: 'bold' }}>
+      <h1 className='cards__header'>At B-JET</h1>
+      <p className='cards__description'>
         Under the mission of "creating people and organizations that challenge the social issues and possibilities of Bangladesh and Miyazaki," we aim to acquire the minimum Japanese language skills necessary for ICT human resources in Bangladesh to work in the ICT market in Japan, as well as the knowledge necessary for living and working in Japan in 20 weeks (400 credit hours).
       </p>
-        <div className='cards__buttons'>
+      <div className='cards__buttons'>
         <Link to="/aboutus">
-          <button>Background & Achievements</button>
+          <button className='cards__button'>Background & Achievements</button>
         </Link>
         <Link to="/program-contents">
-          <button>Program Contents</button>
+          <button className='cards__button'>Program Contents</button>
         </Link>
-        </div>
+      </div>
       <div className='cards__image-link'>
         <a href={linkToPDF} target="_blank" rel="noopener noreferrer">
-          <img src={NewImage} alt="220804v6" />
+          <img src={NewImage} alt="220804v6" className='cards__image-link' />
         </a>
       </div>
-      <div>
-        <h1>B-JET Training Program for it's 14th Batch</h1>
-        <img src={bjet14thBatch} alt="bjet14thBatch" />
+      <h2 className='cards__subheader'>B-JET Training Program for its 14th Batch</h2>
+      <div className='cards__image-container'>
+        <img src={bjet14thBatch} alt="bjet14thBatch" className='cards__image' />
       </div>
     </div>
   );
