37eea4d8e2432d1d416369a58c5f8e1f7ddb7091
nayem73 <nayem73@gmail.com>
2024-04-13 07:20:15 +0600

    populating BjetResourceScreen.js with webinars and success stories

--- Frontend/src/screens/BjetResourceScreen.js
+++ Frontend/src/screens/BjetResourceScreen.js
@@ -2,11 +2,70 @@ import React from 'react';
 import successStory1 from '../images/successStory1.jpg';
 import successStory2 from '../images/successStory2.jpg';
 import successStory3 from '../images/successStory3.jpg';
+import webinar1 from '../images/webinar1.jpg';
+import webinar2 from '../images/webinar2.jpg';
 import './BjetResourceScreen.css';
 
 function BjetResourceScreen() {
     return (
         <div className="container mx-auto px-4 py-8">
+            {/* Card -1 */}
+            <div className="card">
+                <img src={webinar1} alt="Webinar 1" className="card-img-top" />
+                <div className="card-body">
+                    {/* Content for Webinar 1 */}
+                    <h2 className="card-title">Are you interested in pursuing your career in the Japan IT sector?</h2>
+                    <p>Want to know how BJET supports IT grads securing jobs in Japan IT Companies?</p>
+                    <p>Join us TODAY, 1st March for an exclusive B-JET Webinar to learn more about this program!</p>
+                    <h3>Webinar Content:</h3>
+                    <ul>
+                        <li>Introduction & Basic Information about B-JET</li>
+                        <li>About Selection Process of B-JET (Written Exam, Interview, Japanese Language Task)</li>
+                        <li>Q&A</li>
+                    </ul>
+                    <p><strong>Webinar Date:</strong> 01/03/2024</p>
+                    <p><strong>Webinar Time:</strong> 3:00 - 4:00 PM</p>
+                    <p><strong>Meeting ID:</strong> 724 793 3778</p>
+                    <a href="https://bjitgroup.zoom.us/j/7247933778?omn=83198726532" target="_blank" rel="noopener noreferrer">Webinar Link</a>
+                    <p>Secure your spot for this transformative webinar! Don't miss out on the chance to take your career to new heights.</p>
+                    <p>B-JET program is now accepting applications for its 14th batch!</p>
+                    <p><strong>For those who didn’t apply yet,</strong></p>
+                    <a href="https://forms.gle/tzJYJH4PYuSFb3vF8" target="_blank" rel="noopener noreferrer">14th Batch Application/Registration Link</a>
+                    <p><strong>Application deadline:</strong> 21/03/2024</p>
+                    <a href="https://shorturl.at/psBKV" target="_blank" rel="noopener noreferrer">For more information</a>
+                </div>
+            </div>
+            
+            {/* Card 0 */}
+            <div className="card">
+                <img src={webinar2} alt="Webinar 2" className="card-img-top" />
+                <div className="card-body">
+                    <h2 className="card-title">Good News for IT Graduates for Career in Japan IT Companies</h2>
+                    <p>Join us on 16th March for an exclusive B-JET Webinar! Unlock your potential in the Japanese ICT market with the Bangladesh-Japan ICT Engineers' Training Program (B-JET).</p>
+                    <p><strong>Special Guest -</strong></p>
+                    <p>Suman Dash (B-JET 1st Batch Graduate)</p>
+                    <a href="https://www.facebook.com/profile.php?id=100000535335735" target="_blank" rel="noopener noreferrer">Suman Dash's Facebook Profile</a>
+                    <p>He has got 5 years of experience working in Japan IT Company. He is here to share his wonderful experience, insights and information for the interested candidates.</p>
+                    <h3>Webinar Content:</h3>
+                    <ul>
+                        <li>Experience Sharing by Suman San.</li>
+                        <li>Q&A</li>
+                        <li>Introduction & Basic Information about B-JET</li>
+                    </ul>
+                    <p><strong>Webinar Date:</strong> 16/03/2024</p>
+                    <p><strong>Webinar Time:</strong> Start from 10:00 AM</p>
+                    <p><strong>Meeting ID:</strong> 724 793 3778</p>
+                    <a href="https://bjitgroup.zoom.us/j/7247933778?omn=83198726532" target="_blank" rel="noopener noreferrer">Webinar Link</a>
+                    <p>Mark your calendars and secure your spot for this transformative webinar! Don't miss out on the chance to take your career to new heights.</p>
+                    <p>14th batch is going to be the last batch of B-JET this year,</p>
+                    <p><strong>For those who didn’t apply yet,</strong></p>
+                    <a href="https://forms.gle/tzJYJH4PYuSFb3vF8" target="_blank" rel="noopener noreferrer">14th Batch Application/Registration Link</a>
+                    <p><strong>Application deadline:</strong> 21/03/2024</p>
+                    <a href="https://shorturl.at/psBKV" target="_blank" rel="noopener noreferrer">For more information</a>
+                    <p>Apply NOW!!!</p>
+                </div>
+            </div>
+            
             {/* Card 1 */}
             <div className="card">
                 <img src={successStory1} alt="Success Story 1" className="card-img-top" />
