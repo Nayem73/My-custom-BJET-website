4c895ec6b0bec0fb61ac820a4f5ce57c2049aed4
nayem73 <nayem73@gmail.com>
2024-04-13 13:14:58 +0600

    Some changes

--- Frontend/src/components/Alumni.js
+++ Frontend/src/components/Alumni.js
@@ -1,3 +1,4 @@
+// Alumni.js
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './Alumni.css';
@@ -16,6 +17,21 @@ function Alumni() {
         console.error('Error fetching users:', error);
       });
   }, []);
+
+  // Helper function to chunk the array into smaller arrays
+  const chunkArray = (arr, size) => {
+    return arr.reduce((chunks, el, i) => {
+      if (i % size === 0) {
+        chunks.push([el]);
+      } else {
+        chunks[chunks.length - 1].push(el);
+      }
+      return chunks;
+    }, []);
+  };
+
+  const userChunks = chunkArray(users, 4);
+
   return (
     <div className='cards'>
       <h1>B-JET Alumni</h1>
@@ -23,21 +39,23 @@ function Alumni() {
         Our alumni have gone on to work in various IT industries in Japan. They are making a positive impact in their communities and contributing to the growth of the Japanese technology and economy.
       </p>
       <div className='cards__container'>
-        <div className='cards__wrapper'>
-          <ul className='cards__items'>
-            {users.map(user => (
-              <CardItem
-                key={user.id}
-                src={user.profilePicture} // Use the profilePicture provided by your backend
-                text={user.position || 'Software Engineer'} // Use the about provided by your backend
-                name={user.userName || 'BJET Graduate'} // Use the about provided by your backend
-                company={user.company || 'BJET'} // Use the about provided by your backend
-                label={`Batch ${user.bjetBatch}`}
-                path={`/users/${user.id}`} // Replace with the actual path to view user details
-              />
-            ))}
-          </ul>
-        </div>
+        {userChunks.map((chunk, index) => (
+          <div key={index} className='cards__wrapper'>
+            <ul className='cards__items'>
+              {chunk.map(user => (
+                <CardItem
+                  key={user.id}
+                  src={user.profilePicture}
+                  text={user.position || 'Software Engineer'}
+                  name={user.userName || 'BJET Graduate'}
+                  company={user.company || 'BJET'}
+                  label={`Batch ${user.bjetBatch}`}
+                  path={`/users/${user.id}`}
+                />
+              ))}
+            </ul>
+          </div>
+        ))}
       </div>
     </div>
   );
