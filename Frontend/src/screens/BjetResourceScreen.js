b724e13da16a1da8ad9df5d0e50207700656f4b8
nayem73 <nayem73@gmail.com>
2024-04-21 17:50:03 +0600

    Working with BjetResourceScreen.js to add update and delete functionalities.

--- Frontend/src/screens/BjetResourceScreen.js
+++ Frontend/src/screens/BjetResourceScreen.js
@@ -1,112 +1,48 @@
-import React from 'react';
-import successStory1 from '../images/successStory1.jpg';
-import successStory2 from '../images/successStory2.jpg';
-import successStory3 from '../images/successStory3.jpg';
-import webinar1 from '../images/webinar1.jpg';
-import webinar2 from '../images/webinar2.jpg';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 import './BjetResourceScreen.css';
 
 function BjetResourceScreen() {
+    const [reviews, setReviews] = useState([]);
+    const userLogin = useSelector(state => state.userLogin);
+    const { userInfo } = userLogin;
+    const history = useNavigate();
+
+    useEffect(() => {
+        const fetchReviews = async () => {
+            const res = await axios.get('http://localhost:8080/api/review'); // replace with your API endpoint
+            setReviews(res.data.content); // adjust this line according to your API response structure
+        };
+
+        fetchReviews();
+    }, [history, userInfo]);
+
+    const handleEdit = (review) => {
+        if (!userInfo || !userInfo.isAdmin) {
+            alert('Only super admin can edit/update/post reviews.');
+        } else {
+            // handle edit functionality here
+            history(`/editReview/${review.reviewId}`);
+        }
+    };
+
     return (
         <div className="container mx-auto px-4 py-8">
-            {/* Card -1 */}
-            <div className="card">
-                <img src={webinar1} alt="Webinar 1" className="card-img-top" />
-                <div className="card-body">
-                    {/* Content for Webinar 1 */}
-                    <h2 className="card-title">Are you interested in pursuing your career in the Japan IT sector?</h2>
-                    <p>Want to know how BJET supports IT grads securing jobs in Japan IT Companies?</p>
-                    <p>Join us TODAY, 1st March for an exclusive B-JET Webinar to learn more about this program!</p>
-                    <h3>Webinar Content:</h3>
-                    <ul>
-                        <li>Introduction & Basic Information about B-JET</li>
-                        <li>About Selection Process of B-JET (Written Exam, Interview, Japanese Language Task)</li>
-                        <li>Q&A</li>
-                    </ul>
-                    <p><strong>Webinar Date:</strong> 01/03/2024</p>
-                    <p><strong>Webinar Time:</strong> 3:00 - 4:00 PM</p>
-                    <p><strong>Meeting ID:</strong> 724 793 3778</p>
-                    <a href="https://bjitgroup.zoom.us/j/7247933778?omn=83198726532" target="_blank" rel="noopener noreferrer">Webinar Link</a>
-                    <p>Secure your spot for this transformative webinar! Don't miss out on the chance to take your career to new heights.</p>
-                    <p>B-JET program is now accepting applications for its 14th batch!</p>
-                    <p><strong>For those who didn’t apply yet,</strong></p>
-                    <a href="https://forms.gle/tzJYJH4PYuSFb3vF8" target="_blank" rel="noopener noreferrer">14th Batch Application/Registration Link</a>
-                    <p><strong>Application deadline:</strong> 21/03/2024</p>
-                    <a href="https://shorturl.at/psBKV" target="_blank" rel="noopener noreferrer">For more information</a>
+            {reviews.map((review) => (
+                <div className="card" key={review.reviewId}>
+                    <img src={review.img} alt={review.userName} className="card-img-top" />
+                    <div className="card-body">
+                        <h2 className="card-title">{review.userName}</h2>
+                        <p>{review.comment}</p>
+                        {/* Add more fields as per your review structure */}
+                        {userInfo && userInfo.isAdmin && (
+                            <button onClick={() => handleEdit(review)}>Edit</button>
+                        )}
+                    </div>
                 </div>
-            </div>
-            
-            {/* Card 0 */}
-            <div className="card">
-                <img src={webinar2} alt="Webinar 2" className="card-img-top" />
-                <div className="card-body">
-                    <h2 className="card-title">Good News for IT Graduates for Career in Japan IT Companies</h2>
-                    <p>Join us on 16th March for an exclusive B-JET Webinar! Unlock your potential in the Japanese ICT market with the Bangladesh-Japan ICT Engineers' Training Program (B-JET).</p>
-                    <p><strong>Special Guest -</strong></p>
-                    <p>Suman Dash (B-JET 1st Batch Graduate)</p>
-                    <a href="https://www.facebook.com/profile.php?id=100000535335735" target="_blank" rel="noopener noreferrer">Suman Dash's Facebook Profile</a>
-                    <p>He has got 5 years of experience working in Japan IT Company. He is here to share his wonderful experience, insights and information for the interested candidates.</p>
-                    <h3>Webinar Content:</h3>
-                    <ul>
-                        <li>Experience Sharing by Suman San.</li>
-                        <li>Q&A</li>
-                        <li>Introduction & Basic Information about B-JET</li>
-                    </ul>
-                    <p><strong>Webinar Date:</strong> 16/03/2024</p>
-                    <p><strong>Webinar Time:</strong> Start from 10:00 AM</p>
-                    <p><strong>Meeting ID:</strong> 724 793 3778</p>
-                    <a href="https://bjitgroup.zoom.us/j/7247933778?omn=83198726532" target="_blank" rel="noopener noreferrer">Webinar Link</a>
-                    <p>Mark your calendars and secure your spot for this transformative webinar! Don't miss out on the chance to take your career to new heights.</p>
-                    <p>14th batch is going to be the last batch of B-JET this year,</p>
-                    <p><strong>For those who didn’t apply yet,</strong></p>
-                    <a href="https://forms.gle/tzJYJH4PYuSFb3vF8" target="_blank" rel="noopener noreferrer">14th Batch Application/Registration Link</a>
-                    <p><strong>Application deadline:</strong> 21/03/2024</p>
-                    <a href="https://shorturl.at/psBKV" target="_blank" rel="noopener noreferrer">For more information</a>
-                    <p>Apply NOW!!!</p>
-                </div>
-            </div>
-            
-            {/* Card 1 */}
-            <div className="card">
-                <img src={successStory1} alt="Success Story 1" className="card-img-top" />
-                <div className="card-body">
-                    <h2 className="card-title">B-JET Success Story: From Classroom to Career in Japan IT Sector!</h2>
-                    <p>Hear from our 13th Batch trainee, one of our accomplished graduates, A proud member of the B-JET family.</p>
-                    <p>B-JET program is now accepting applications for its 14th batch!</p>
-                    <p><strong>Registration Link:</strong> <a href="https://forms.gle/tzJYJH4PYuSFb3vF8">https://forms.gle/tzJYJH4PYuSFb3vF8</a></p>
-                    <p><strong>Application deadline:</strong> 21/03/2024</p>
-                    <p><strong>For more information:</strong> <a href="https://shorturl.at/psBKV">https://shorturl.at/psBKV</a></p>
-                    <p>Join us in celebrating their success and get inspired by their remarkable journey! If you're ready to carve your path to IT Career in Japan, then B-JET is the perfect platform for you! More than 200 B-JET graduates are now professionally working as global talents in reputed Japanese companies.</p>
-                </div>
-            </div>
-
-            {/* Card 2 */}
-            <div className="card mt-8">
-                <img src={successStory2} alt="Success Story 2" className="card-img-top" />
-                <div className="card-body">
-                    <h2 className="card-title">B-JET Success Story: From Classroom to Career in Japan IT Sector!</h2>
-                    <p>Hear from our 13th Batch trainee, one of our accomplished graduates, A proud member of the B-JET family.</p>
-                    <p>B-JET program is now accepting applications for its 14th batch!</p>
-                    <p><strong>Registration Link:</strong> <a href="https://forms.gle/tzJYJH4PYuSFb3vF8">https://forms.gle/tzJYJH4PYuSFb3vF8</a></p>
-                    <p><strong>Application deadline:</strong> 21/03/2024</p>
-                    <p><strong>For more information:</strong> <a href="https://shorturl.at/psBKV">https://shorturl.at/psBKV</a></p>
-                    <p>Join us in celebrating their success and get inspired by their remarkable journey! If you're ready to carve your path to IT Career in Japan, then B-JET is the perfect platform for you! More than 200 B-JET graduates are now professionally working as global talents in reputed Japanese companies.</p>
-                </div>
-            </div>
-
-            {/* Card 3 */}
-            <div className="card mt-8">
-                <img src={successStory3} alt="Success Story 3" className="card-img-top" />
-                <div className="card-body">
-                    <h2 className="card-title">B-JET Success Story: From Classroom to Career in Japan IT Sector!</h2>
-                    <p>Hear from our 13th Batch trainee, one of our accomplished graduates, A proud member of the B-JET family.</p>
-                    <p>B-JET program is now accepting applications for its 14th batch!</p>
-                    <p><strong>Registration Link:</strong> <a href="https://forms.gle/tzJYJH4PYuSFb3vF8">https://forms.gle/tzJYJH4PYuSFb3vF8</a></p>
-                    <p><strong>Application deadline:</strong> 21/03/2024</p>
-                    <p><strong>For more information:</strong> <a href="https://shorturl.at/psBKV">https://shorturl.at/psBKV</a></p>
-                    <p>Join us in celebrating their success and get inspired by their remarkable journey! If you're ready to carve your path to IT Career in Japan, then B-JET is the perfect platform for you! More than 200 B-JET graduates are now professionally working as global talents in reputed Japanese companies.</p>
-                </div>
-            </div>
+            ))}
         </div>
     );
 }
