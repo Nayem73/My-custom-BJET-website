a3d1fd6e644c94f998dbf9e3bee167a5b2648c4b
nayem73 <nayem73@gmail.com>
2024-04-24 11:26:36 +0600

    Added a Japanese translation for Breaking the Barrier motto

--- Frontend/src/screens/BreakTheBarrier.js
+++ Frontend/src/screens/BreakTheBarrier.js
@@ -9,6 +9,7 @@ function BreakTheBarrier() {
                 <div className="btb-card-body">
                     <h2 className="btb-card-title" style={{ fontSize: '28px' }}>Break the Barrier</h2>
                     <p className="btb-highlighted-text">This section should contain unique learning roadmap, steps, materials and resources in an organized format that will help narrow the gap between Bangla and Japanese language and culture.</p>
+                    <p className="btb-highlighted-text">このセクションには、バングラ語と日本の言語と文化の間のギャップを縮めるのに役立つ、独自の学習ロードマップ、ステップ、教材、リソースが体系化された形式で含まれている必要があります。</p>
                     
                     <div className="btb-pdf-container">
                         <iframe src={Hiragana} title="Hiragana PDF" className="btb-pdf-viewer"></iframe>
